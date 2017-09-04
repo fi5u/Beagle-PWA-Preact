3760003b8a2e6472ce0498c6487db0137862c706
Tommy Fisher <tommybfisher@gmail.com>
2017-09-04 21:16:59 +0300

    Basic styles for help page

--- src/routes/help/index.js
+++ src/routes/help/index.js
@@ -1,9 +1,25 @@
 import { h, Component } from 'preact'
+import { style } from 'typestyle'
 
 export default class Help extends Component {
+    constructor() {
+        super()
+
+        this.classNames = this.classNames()
+    }
+
+    classNames() {
+        return {
+            page: style({
+                color: '#fff',
+                padding: '0 22px',
+            }),
+        }
+    }
+
     render() {
         return (
-            <div>
+            <div className={this.classNames.page}>
                 <h1>Help</h1>
                 <p>This is the Help component.</p>
             </div>
