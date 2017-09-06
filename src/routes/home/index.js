d284238d4ca0c52dace99bb2a73611db3ceddaa7
Tommy Fisher <tommybfisher@gmail.com>
2017-09-06 21:05:35 +0300

    Fake data created

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,6 +2,31 @@ import { h, Component } from 'preact'
 import style from './style'
 
 export default class Home extends Component {
+    constructor() {
+        super()
+
+        this.data = [
+            {
+                spaceSymbol: '+',
+                title: 'House of Fraser',
+                url: 'https://www.houseoffraser.co.uk',
+                urlTemplate: 'https://www.houseoffraser.co.uk/search/$',
+            },
+            {
+                spaceSymbol: '%20',
+                title: 'Bing',
+                url: 'https://bing.com',
+                urlTemplate: 'https://bing.com/?s=$',
+            },
+            {
+                spaceSymbol: '+',
+                title: 'Google',
+                url: 'https://google.com',
+                urlTemplate: 'https://google.com/search/?q=$',
+            },
+        ]
+    }
+
     render() {
         return (
             <div class={style.home}>
